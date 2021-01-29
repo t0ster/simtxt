d3162a107fb5fd538df935c34a4c78ea3eecf3f8
Roman Dolgyi <roman@btsolutions.co>
2021-01-29 04:53:38 +0200

    Fix

--- server/tests/test_app.py
+++ server/tests/test_app.py
@@ -2,6 +2,7 @@ from simtxt.app import app
 
 
 async def test_app(aiohttp_client, loop):
+    app.on_startup.clear()
     client = await aiohttp_client(app)
     resp = await client.get("/graphiql")
     assert resp.status == 200
